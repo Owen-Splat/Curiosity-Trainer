79ec689ae25277044dfccb6fbe8d2e175c58f233
Owen Smart <Owen32Smart@gmail.com>
2025-10-19 12:56:35 -0400

    read/write data using structures

--- manager.py
+++ manager.py
@@ -1,12 +1,14 @@
-import ctypes, win32ui, win32process, win32api
+import ctypes, math, win32ui, win32process, win32api
 from mem_edit import Process
+import time
 
 
 class CuriosityManager:
     PID = 0
     BASE_ADDRESS = 0x00000000
-    LIFE_PTR = (0x080A8F50, (0x30, 0xB0, 0x70, 0x830, 0x48, 0x0, 0x44))
-    POS_PTR = (0x080868A0, (0x30, 0x190, 0x10, 0x218, 0x430, 0xB0, 0x204)) # TODO: THIS POINTER IS NOT FULLY STATIC
+    LIFE_PTR = (0x080A8F50, (0x30, 0xB0, 0x70, 0x830, 0x48, 0x0, 0x44)) # might also not be static, I had gotten it down to 12 pointers and chosen the first one
+    POS_PTR = (0x07F28FF0, (0xD30, 0x0, 0xBC0, 0x310, 0x198, 0xB0, 0x200))
+    # POS_PTR = (0x080868A0, (0x30, 0x190, 0x10, 0x218, 0x430, 0xB0, 0x204)) # TODO: THIS POINTER IS NOT FULLY STATIC
     POS_SCALE = 100
 
 
@@ -45,21 +47,53 @@ class CuriosityManager:
 
 
     def readPosition(self) -> tuple:
-        addr = self.getPTRAddr(self.POS_PTR) - 0x4
-        z = self.game.read_memory(addr, ctypes.c_double())
-        y = self.game.read_memory(addr - 8, ctypes.c_double())
-        x = self.game.read_memory(addr - 16, ctypes.c_double())
-        return (x.value / self.POS_SCALE, y.value / self.POS_SCALE, z.value / self.POS_SCALE)
+        addr = self.getPTRAddr(self.POS_PTR)
+        pos = self.game.read_memory(addr - 0x10, Vector3())
+        return (pos.X / self.POS_SCALE, pos.Y / self.POS_SCALE, pos.Z / self.POS_SCALE)
 
 
-    def writePosition(self, pos: tuple) -> None:
-        addr = self.getPTRAddr(self.POS_PTR) - 0x4
-        self.game.write_memory(addr, ctypes.c_double(pos[2] * self.POS_SCALE))
-        self.game.write_memory(addr - 8, ctypes.c_double(pos[1] * self.POS_SCALE))
-        self.game.write_memory(addr - 16, ctypes.c_double(pos[0] * self.POS_SCALE))
+    def writePosition(self, _pos: tuple) -> None:
+        addr = self.getPTRAddr(self.POS_PTR)
+
+        # set position
+        pos = Vector3(
+            _pos[0] * self.POS_SCALE,
+            _pos[1] * self.POS_SCALE,
+            _pos[2] * self.POS_SCALE
+        )
+        self.game.write_memory(addr - 0x10, pos)
+
+        # # reset angle - NOT WORKING
+        # self.game.write_memory(addr - 0x30, Vector3(0, 0, 0))
+
+        # # reset velocity - NOT WORKING
+        # self.game.write_memory(addr - 0x90, Vector3(0, 0, 0))
 
 
-    def refillBedsLives(self) -> None:
-        addr = self.getPTRAddr(self.LIFE_PTR)
-        self.game.write_memory(addr, ctypes.c_int(999))
-        self.game.write_memory(addr + 8, ctypes.c_int(999))
+class SaveData(ctypes.Structure):
+    _fields_ = [
+        ("TimesSlept", ctypes.c_int32),
+        ("SleepsRemaining", ctypes.c_int32), # LIFE_PTR result
+        ("TimesMadeBed", ctypes.c_int32),
+        ("BedsRemaining", ctypes.c_int32),
+        ("PlayedIntro", ctypes.c_bool),
+        ("UnlockedSleeping", ctypes.c_bool),
+        ("ZonesEntered", ctypes.c_int32),
+        ("ZonesFellFrom", ctypes.c_int32),
+        ("NonCafeZonesEntered", ctypes.c_int32),
+        ("WakeUpId", ctypes.c_int32)
+    ]
+    # BED_OBJECT_PATH = ""
+    # BED_OBJECT_OFFSET = None # Vector
+    # BED_OBJECT_ROTATION = 0.0 # Double
+    # BED_ZONE_INDEX = 0
+    # TRIGGERED_DIALOGUE_AUDIO_TRIGGERS = None # set property, int, maybe a bitmask?
+    # TIMES_SPAWNED = 0
+
+
+class Vector3(ctypes.Structure):
+    _fields_ = [
+        ("X", ctypes.c_double),
+        ("Y", ctypes.c_double),
+        ("Z", ctypes.c_double)
+    ]
